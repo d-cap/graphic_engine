3f9871453f7750e8a29ae8d1de0e385ffb2e8aab
Davide Capomagi <davide.capomagi@gmail.com>
2022-05-29 08:28:39 +0100

    Added triangle

--- src/main.rs
+++ src/main.rs
@@ -47,12 +47,102 @@ fn main() {
         gl::DepthFunc(gl::LESS);
     }
 
+    let mut attributes = 0;
+    unsafe { gl::GetIntegerv(gl::MAX_VERTEX_ATOMIC_COUNTERS, &mut attributes) };
+    println!("Attrib: {}", attributes);
+
     let vertices: Vec<f32> = vec![
         -0.5, -0.5, 0., //
         0.5, -0.5, 0., //
         0., 0.5, 0.,
     ];
 
+    let vertices_ebo: Vec<f32> = vec![
+        0.5, 0.5, 0.0, // top right
+        0.5, -0.5, 0.0, // bottom right
+        -0.5, -0.5, 0.0, // bottom left
+        -0.5, 0.5, 0.0, // top left
+    ];
+    let indices: Vec<u32> = vec![
+        // note that we start from 0!
+        0, 1, 3, // first triangle
+        1, 2, 3, // second triangle
+    ];
+
+    let vertex_shader_source = std::ffi::CString::new(
+        "
+        #version 330 core
+        layout (location = 0) in vec3 aPos;
+
+        void main()
+        {
+            gl_Position = vec4(aPos, 1.0f);
+        }
+    ",
+    )
+    .unwrap();
+    let vertex_shader = compile_shader(vertex_shader_source, gl::VERTEX_SHADER);
+
+    let fragment_shader_source = std::ffi::CString::new(
+        "
+        #version 330 core
+        out vec4 FragColor;
+
+        void main()
+        {
+            FragColor = vec4(0.5f, 0.1f, 0.3f, 1.0f);
+        }
+    ",
+    )
+    .unwrap();
+    let fragment_shader = compile_shader(fragment_shader_source, gl::FRAGMENT_SHADER);
+
+    let program;
+    unsafe {
+        program = gl::CreateProgram();
+        gl::AttachShader(program, vertex_shader);
+        gl::AttachShader(program, fragment_shader);
+        gl::LinkProgram(program);
+        let mut success = 0;
+        gl::GetProgramiv(program, gl::LINK_STATUS, &mut success);
+        if success == 0 {
+            panic!("Linking failed");
+        }
+        gl::DeleteShader(vertex_shader);
+        gl::DeleteShader(fragment_shader);
+    }
+
+    let mut vao = 0;
+    unsafe {
+        gl::GenVertexArrays(1, &mut vao);
+        gl::BindVertexArray(vao);
+        let mut vbo = 0;
+        gl::GenBuffers(1, &mut vbo);
+
+        gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
+        gl::BufferData(
+            gl::ARRAY_BUFFER,
+            (vertices.len() * std::mem::size_of::<f32>()) as _,
+            vertices.as_ptr() as _,
+            gl::STATIC_DRAW,
+        );
+    }
+
+    unsafe {
+        gl::VertexAttribPointer(
+            0,
+            3,
+            gl::FLOAT,
+            gl::FALSE,
+            (3 * std::mem::size_of::<f32>()) as _,
+            0 as _,
+        );
+        gl::EnableVertexAttribArray(0);
+        gl::BindBuffer(gl::ARRAY_BUFFER, 0);
+        gl::BindVertexArray(0);
+    }
+
+    let cstr = std::ffi::CString::new("ourColor").unwrap();
     while running {
         let milliseconds = timer.ticks();
         let start = timer.performance_counter();
@@ -134,8 +224,12 @@ fn main() {
 
         // Render
         unsafe {
-            gl::ClearColor(0.5, 0., 0.5, 1.);
+            gl::ClearColor(0.2, 0.5, 0., 1.);
             gl::Clear(gl::COLOR_BUFFER_BIT | gl::DEPTH_BUFFER_BIT);
+            gl::UseProgram(program);
+            gl::BindVertexArray(vao);
+            gl::DrawArrays(gl::TRIANGLES, 0, 3);
+            gl::BindVertexArray(0);
         };
 
         let end = timer.performance_counter();
@@ -149,4 +243,19 @@ fn main() {
 
         window.gl_swap_window();
     }
+}
+
+fn compile_shader(source: std::ffi::CString, shader_type: gl::types::GLenum) -> gl::types::GLuint {
+    let shader;
+    unsafe {
+        shader = gl::CreateShader(shader_type);
+        gl::ShaderSource(shader, 1, &source.as_ptr(), std::ptr::null());
+        gl::CompileShader(shader);
+        let mut success = 0;
+        gl::GetShaderiv(shader, gl::COMPILE_STATUS, &mut success);
+        if success == 0 {
+            panic!("Compiling failed");
+        }
+    }
+    shader
 }
